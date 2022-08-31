126ef51c36ad2308f42dfc452eb275148a70cfb6
vannidar <vannidar@DESKTOP-GQAU12N.localdomain>
2022-08-31 15:47:06 -0700

    Finished 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(ogStr, numRep) {
+    if (numRep < 0) return "ERROR";
+    let str = "";
+    for (let i = 0; i < numRep; i++) str += ogStr;
+    return str;
 };
 
 // Do not edit below this line
