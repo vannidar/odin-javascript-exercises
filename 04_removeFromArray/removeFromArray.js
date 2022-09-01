fb7ce39cbfeff45be7bcdca32eb73db43b946b1b
vannidar <vannidar@DESKTOP-GQAU12N.localdomain>
2022-08-31 17:35:54 -0700

    Solved 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...valsToRemove) {
+    /*
+    console.log(arr);
+    for (let i = 0; i < valsToRemove.length; i++) {
+        elementIndex = arr.findIndex((element) => element === valsToRemove[i])
+        if (elementIndex >= 0) {
+            console.log(`Found match at index ${elementIndex}`);
+            arr.splice(elementIndex, 1);
+        }
+    }
+    console.log(arr);
+    */
+    return arr.filter((element) => !valsToRemove.includes(element));
 };
 
 // Do not edit below this line
