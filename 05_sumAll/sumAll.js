43c0699ef4c9c244e58aed134e6f0a2f7c5b73e8
vannidar <vannidar@DESKTOP-GQAU12N.localdomain>
2022-08-31 17:55:10 -0700

    Solved 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second) || first < 0 || second < 0) return "ERROR";
+    let sum = 0;
+    const end = Math.max(first, second);
+    for (let i = Math.min(first, second); i <= end; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
